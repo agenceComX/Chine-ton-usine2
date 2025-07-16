5398420a8ed6322044dce4176503f8a20f42c4fe
agenceComX <comx.agence@gmail.com>
2025-07-16 01:08:00 +0100

    Added ignore

--- dist/assets/useToastContext-TG_IWIBh.js
+++
@@ -1,1 +0,0 @@
-import{r as o,Y as e}from"./index-DbrDj4Du.js";const r=()=>{const t=o.useContext(e);if(t===void 0)throw new Error("useToastContext must be used within a ToastProvider");return t};export{r as u};
--- src/lib/services/productServiceInstance.ts
+++
@@ -1,5 +0,0 @@
-// Instance du service produit
-import { ProductService } from './productService';
-
-export const productService = new ProductService();
-export default productService;
